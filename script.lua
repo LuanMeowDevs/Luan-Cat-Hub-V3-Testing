596694fe794d3998386f857d4450a4b1ecfb209d
LuanMeowDevs <jefsilvafranca.gr@hotmail.com>
2026-02-19 19:03:23 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -2389,7 +2389,7 @@ QuestNeta = function()
 			[6] = PosQ,
 		};
 	end;
-	local Library = loadstring(game:HttpGet("https://pastefy.app/5uHygRVT/raw"))():MakeWindow({
+	local Library = loadstring(game:HttpGet("https://pastefy.app/NbkyRa3q/raw"))():MakeWindow({
     Title = "Luan Cat Hub",
     SubTitle = "by Luan Cat And 1x1x1x1x1",
     SaveFolder = "M3Ow.json"
